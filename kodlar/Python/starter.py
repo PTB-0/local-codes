40c2651550af3252427d87ee4b24aaad10a20e59
ege <ege.yildiz123456789@gmail.com>
2025-10-10 09:38:19 +0300

    dizin güncellemesi optimizasyonu
    dosya ağacı değiştiği için ayarlamalar yapıldı

--- kodlar/Python/starter.py
+++ kodlar/Python/starter.py
@@ -3,24 +3,24 @@ import os
 control = input("hesap makinesi 1 site açma 2 hey 3 heyo 4 bat 5 xox 6")
 
 if control == "1" :
-    os.system (" C:\\Users\\USER\\Desktop\\kodlar\\Python\\hesapMakinesi.py")
+    os.system (" C:\\Users\\USER\\Desktop\\local-codes\\kodlar\\Python\\hesapMakinesi.py")
 elif control == "2" :
-    os.system(" C:\\Users\\USER\\Desktop\\kodlar\\Python\\siteAcma.py")
+    os.system(" C:\\Users\\USER\\Desktop\\local-codes\\kodlar\\Python\\siteAcma.py")
 elif control == "3" :
-    os.system(" C:\\Users\\USER\\Desktop\\kodlar\\Python\\hey.py")
+    os.system(" C:\\Users\\USER\\Desktop\\local-codes\\kodlar\\Python\\hey.py")
 elif control == "4" :
-    os.system(" C:\\Users\\USER\\Desktop\\kodlar\\Python\\heyo.py")
+    os.system(" C:\\Users\\USER\\Desktop\\local-codes\\kodlar\\Python\\heyo.py")
 elif control == "5" :
-    os.system("C:\\Users\\USER\\Desktop\\kodlar\\bat.bat")
+    os.system("C:\\Users\\USER\\Desktop\\local-codes\\kodlar\\bat.bat")
 elif control == "6" :
-    os.system("C:\\Users\\USER\\Desktop\\kodlar\\Python\\xox.py")
+    os.system("C:\\Users\\USER\\Desktop\\local-codes\\kodlar\\Python\\xox.py")
 else :
     if control != "0":
-        os.system("C:\\Users\\USER\\Desktop\\kodlar\\Python\\starter.py")
+        os.system("C:\\Users\\USER\\Desktop\\local-codes\\kodlar\\Python\\starter.py")
     else:
         print("sanırım yanlış girdin ")
         time.sleep(3)
-        os.system(" C:\\Users\\USER\\Desktop\\kodlar\\Python\\starter.py")
+        os.system(" C:\\Users\\USER\\Desktop\\local-codes\\kodlar\\Python\\starter.py")
         print("3")
         time.sleep(1)
         print("2")
