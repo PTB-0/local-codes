5ca66e6fe5504547c669518247a277bac3f2074f
ege <ege.yildiz123456789@gmail.com>
2025-10-10 15:02:09 +0300

    fibonacci.py ye güncelleme yapıldı
    geliştirildi bir mod daha eklendi

--- kodlar/Python/fibonacci.py
+++ kodlar/Python/fibonacci.py
@@ -1,15 +1,45 @@
+global ilk
+global ikinci
+global i
+global n
+i = 0
+n = 0
+def nBasamak() :
+    ilk = 0
+    ikinci = 1
+    for i in range(n) :
+        ilk = ilk + ikinci
+        ikinci = ilk + ikinci
+    print(ilk)
 while True :
     ilk = 0
     ikinci = 1
-    tekrar = int(input("kaç kere tekrar etsin")) #n yerine tekrar adlı bir değişkeni kullanmanın daha açıklşayıcı olduğunu düşünüyorum
-    for i in range(tekrar) :
-        print(ilk)
-        print(ikinci)
-        ilk = ilk + ikinci 
-        ikinci = ilk + ikinci
-    devam = input("devam etmek istermisiniz")
-    if devam == "evet" or devam == "olur" or devam == "isterim" :
-        continue
-    else :
-        print("çıkış yapılıyor")
-        break
+    mod = input("mod nasıl olsun ? yani ninci tekrardaki sayıyımı söylesin(1) yoksa n e gelesiye kadarmı (2)")
+    if mod == "2" or mod == "(2)" :
+        tekrar = int(input("kaç kere tekrar etsin")) #n yerine tekrar adlı bir değişkeni kullanmanın daha açıklşayıcı ve daha düzgün olduğunu düşünüyorum
+        for i in range(tekrar) :
+            print(ilk)
+            print(ikinci)
+            ilk = ilk + ikinci 
+            ikinci = ilk + ikinci
+        devam = input("devam etmek istermisiniz")
+        if devam == "evet" or devam == "olur" or devam == "isterim" :
+            continue
+        else :
+            print("çıkış yapılıyor")
+            break
+    elif mod == "1" or mod == "(1)" :
+        while True :
+            n = int(input("kaçıncı sayıyı görmek istersiniz"))
+            if n > 100 :
+                print("Dikkat edin! Bu sayı çok uzun olacağı için bilgisiyar yavaşlaya bilir hatta çökebilir önerim başka bir sayı seçmek")
+                kabul = input("sayıyı değiştirecekmisiniz edicekmisiniz? : \n")
+                if kabul == "evet" :
+                    print("tekrarlanıyor ...")
+                else :
+                    nBasamak()
+            elif n < 0 :
+                print("sayı negatif olamaz")
+            else :
+                nBasamak()
+            
