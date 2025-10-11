a0a31bfe97335bf329864e685c3c1027f23800e2
ege <ege.yildiz123456789@gmail.com>
2025-10-12 01:31:45 +0300

    Update statsCOM.py
    eklemeler

--- kodlar/Python/statsCOM.py
+++ kodlar/Python/statsCOM.py
@@ -3,7 +3,7 @@ import psutil
 global a
 
 
-print(" 1 - disk kullanımı \n 2 - Swap usage \n 3 - Cpu usage \n 4 - cores \n 5 - cpu count")
+print(" 1 - disk kullanımı \n 2 - Swap usage \n 3 - Cpu usage \n 4 - cores \n 5 - cpu count \n 6-  boot time \n 7 - disk partları")
 a = input("Ne görmek istersiniz \n :")
 if a == "disk kullanımı" or a == "1" :
     yol = input("hangi diskin kullanımı görmek istersiniz ? \n örneğin c diski için C: yazın\n").upper
@@ -16,6 +16,7 @@ elif a == "4" or a == "cores" :
     print(psutil.cpu_times())
 elif a == "5" or a == "cpu count" :
     print(psutil.cpu_count(False))
-elif a == "6" or a == "boot" or a == :
-    psutil.boot_time
+elif a == "6" or a == "boot" or a == "boot time":
+    print(psutil.boot_time)
 elif a == "7" :
+    print(psutil.disk_partitions)
