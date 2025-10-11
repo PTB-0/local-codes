4ec68de5d8d9c3164ae20c69d6c261ed6facea4b
ege <ege.yildiz123456789@gmail.com>
2025-10-12 01:28:59 +0300

    statsCOM.py
    eklemeler geldi düzeltiler yapıldı

--- kodlar/Python/kisiselSozluk.py
+++ kodlar/Python/kisiselSozluk.py
@@ -1,2 +1,23 @@
+import os
+
+if os.path.exists("sözlük") == False :
+    os.mkdir("sözlük")
+
 print("Kişisel sözlüğe hoş geldiniz")
-mod = input("")
+while True:
+    mod = input("Ne yapmak istersiniz ? \n")
+    if mod == "yazmak" :
+        kelime = input("Kelimeniz nedir? \n")
+        pathK = kelime + ".txt"
+        if os.path.exists("C:\\Users\\USER\\Desktop\\local-codes\\sözlük\\" + pathK) :
+            print("Böyle bir kelime var")
+            do = input("ne yapmak istersiniz")
+            if do == "silmek" :
+                os.remove(pathK)
+                print("başarı ile silindi")
+            elif do == "eklemek" :
+                pathTam = "C:\\Users\\USER\\Desktop\\local-codes\\sözlük\\" + pathK
+                yazi = input("\n ne yazmak istersiniz ? \n")
+                f = open(pathTam , "+a")
+                f.write(yazi)
+#çalışmıyor dosya döngüsü var
--- kodlar/Python/statsCOM.py
+++ kodlar/Python/statsCOM.py
@@ -1,12 +1,21 @@
 import time
 import psutil
 global a
-print(" 1 - disk kullanımı \n 2 - Swap usage \n 3 - Cpu usage \n 4 - cores ")
+
+
+print(" 1 - disk kullanımı \n 2 - Swap usage \n 3 - Cpu usage \n 4 - cores \n 5 - cpu count")
 a = input("Ne görmek istersiniz \n :")
 if a == "disk kullanımı" or a == "1" :
     yol = input("hangi diskin kullanımı görmek istersiniz ? \n örneğin c diski için C: yazın\n").upper
-    b = psutil.disk_usage(yol)
-    print(b)
+    print(psutil.disk_usage(yol))
 elif a == "2" or a == "Swap" or a == "Swap usage" :
-    b = psutil.swap_memory()
-    print(b)
+    print(psutil.swap_memory())
+elif a == "3" or a == "cpu" or a == "cpu usage"  :
+    print(psutil.cpu_stats)
+elif a == "4" or a == "cores" :
+    print(psutil.cpu_times())
+elif a == "5" or a == "cpu count" :
+    print(psutil.cpu_count(False))
+elif a == "6" or a == "boot" or a == :
+    psutil.boot_time
+elif a == "7" :
