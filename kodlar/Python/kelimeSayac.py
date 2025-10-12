145343343df37ea107a8bae478287e50f87b6959
ege <ege.yildiz123456789@gmail.com>
2025-10-12 18:40:21 +0300

    odev4sayac
    ödev 4 yapıldı

---
+++ kodlar/Python/kelimeSayac.py
@@ -0,0 +1,7 @@
+from collections import Counter
+def kelimeSayici(cumle) :
+    cumleUzunluk = len(cumle)
+    print("cümleniz : \n" , cumle , "\n uzunluğu :" , cumleUzunluk )
+
+cumle = input("cümlenizi giriniz")
+kelimeSayici(cumle)
