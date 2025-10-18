73e36820e20ba68eb6d52d45d1f9ae1bec40182d
ege <PTB@projecthebest.com>
2025-10-19 00:40:54 +0300

    Update emailSender.py

--- kodlar/Python/emailSender.py
+++ kodlar/Python/emailSender.py
@@ -6,6 +6,6 @@ Nsifre = ""
 while True :
     mesaj = input("mesajınız nedir")
     mailAdress = input("e mail adreslerinden birini seçiniz")
-    if  mailAdress != "ege.yildiz123456789" or mailAdress != "noloopreal@gmail.com" :
+    if  mailAdress != "balh" or mailAdress != "bleh" :
         print("üzgünüm ama bu e maie izin verilmiyor")
     
