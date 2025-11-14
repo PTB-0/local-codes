2882bd9b006ae31c997ac65cf4103de4e6d22b7f
ege <PTB@projecthebest.com>
2025-11-15 00:54:16 +0300

    Pv1.0.0

--- kodlar/Python/randomSelectionReal.py
+++ kodlar/Python/randomSelectionReal.py
@@ -13,30 +13,32 @@ def Rasgele(tekrar = None) :
             break
     for i in range(tekrar) :
         a = salla.randint(1,100)
-        if list.count(a) > 1 :
+        if tekrarlar.count(a) >= 1 :
             rep = {
                 "sayı" : a ,
-                "tekrar" : list.count(a),
+                "tekrar" : tekrarlar.count(a),
                 "skor" : score
                 }
             tekrarlar.append(rep)
         else :
-            list.append(a)
-    print(list)
+            tekrarlar.append(a)
+    print(tekrarlar)
 def kacTekrar() :
     for rep in tekrarlar:
         print(rep)
     print(score)
 def calis():
     while True :
-        if score == 0:
-            ask = int(input("kaç tekrar olsun (çıkmak için 0 yazınız)"))
+        if score == 0 :
+            ask = int(input("Kaç kere sayı oluşturulsun (0 uygulamadan çıkartır)"))
         else :
-            ask = int(input("tekrar sayısı girmek için o sayıyı girin eğer çıkmak istiyorsanız 0 girin eğer tekrarı görmek istiyorsan -1 gir"))
-            if ask == -1 :
-                kacTekrar()
-        if ask != 0 :
+            ask = int(input("-1 girerseniz kaç tane tekrar olduğunu görürsünüz 0 girerseniz uygulamadan çıkarsınız bir sayı girersen ve o sayı \n bu sayılardan farklı olursa o kadar sayı eklenir"))
+            
+        if ask == -1 :
+            kacTekrar()
+        elif ask == 0 :
+            quit()
+        else :
             Rasgele(ask)
-        else :
-            break
+        
 calis()
