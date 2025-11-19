401f4e0892606b5f9006fe1cc1351af9120ddf57
ege <PTB@projecthebest.com>
2025-11-19 21:27:03 +0300

    LOGIN /v1

---
+++ kodlar/java/uzaklikOlcme/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner ;
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("x1 i girinixz : ");
+        double x1 in.nextDouble()
+    }
+}
