885cec11953602f4d218e39fa49270aa20aa3046
ege <PTB@projecthebest.com>
2025-11-21 19:25:42 +0300

    Update otomatikServerJarPurpur.py

--- kodlar/Python/forServer/otomatikServerJarPurpur.py
+++ kodlar/Python/forServer/otomatikServerJarPurpur.py
@@ -1,6 +1,10 @@
 import webbrowser
 print("Purpur için otomatik jar indirmeye hosgeldiniz lutfen versiyonu girin\n")
-ver = input("Versiyon")
-linkPurpur = "https://api.purpurmc.org/v2/purpur/"
-linkPurpurC = "/latest/download"   #bu purpurun indirme apisi
-webbrowser.open(linkPurpur+ver+linkPurpurC)
+while True :
+    ver = input("Versiyon")
+    linkPurpur = "https://api.purpurmc.org/v2/purpur/"
+    linkPurpurC = "/latest/download"   #bu purpurun indirme apisi
+    webbrowser.open(linkPurpur+ver+linkPurpurC)
+    ask = input("durdurmak istermisin")
+    if ask == "evet" or ask == "stop" :
+        break
