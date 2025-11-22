95f0b2de1caf079b1195b1dedbcb8d93ea926087
ege <PTB@projecthebest.com>
2025-11-22 03:24:00 +0300

    v1

---
+++ kodlar/Python/forServer/otomasyonJarPull.py
@@ -0,0 +1,8 @@
+def verTake(ver = None):
+    while True :
+        if ver == None :
+            ver = input("VERSIYONU girin")
+            if ver != None :
+                break
+    else :
+        
