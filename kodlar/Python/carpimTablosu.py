982f3ac883d49f0c2bd4be6186ffb0e6f0a5d003
ege <PTB@projecthebest.com>
2025-12-03 21:30:49 +0300

    v2

--- kodlar/Python/carpimTablosu.py
+++ kodlar/Python/carpimTablosu.py
@@ -1,11 +1,28 @@
+def mod1():
+    while True :
+        try  : 
+            istekSayi = int(input("bir sayı giriniz"))
+        except ValueError :
+            print("düzgüce bir sayı girmeniz lazım")
+            continue
+        if istekSayi <= 10 and istekSayi >= 0 :
+            for i in range(11) :
+                print(istekSayi * i)
+                i += 1
+def mod2():
+    while True :
+        num = int(input(""))
 
-while True :
-    try :
-        istekSayi = int(input("bir sayı giriniz"))
-    except ValueError :
-        print("düzgüce bir sayı girmeniz lazım")
-        continue
-    if istekSayi <= 10 and istekSayi >= 0 :
-        for i in range(11) :
-            print(istekSayi * i)
-            i += 1
+def mod3() :
+    for i in range (100):
+        j = i * i
+        print( i , j) 
+def secenek():
+    ask = input("mod 1 mod 2 mod 3(write as mod1 mod2 mod3)")
+    if ask == "mod1" :
+        mod1()
+    elif ask == "mod2" :
+        mod2()
+    elif ask == "mod3" :
+        mod3()
+secenek()
